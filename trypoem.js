46eb87cae237144af29aecb54adf9ee5db13d440
Zolevantsss <emmanuelsol9302004@gmail.com>
2024-08-21 15:18:15 +0800

    Mini Project Added

---
+++ trypoem.js
@@ -0,0 +1,38 @@
+console.log(`Welcome to Poem Line Reader!
+TITLE  : ORACLE
+AUTHOR : DURIEL E. HARRIS\n`);
+
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function promptForLineNumber() {
+  rl.question('Enter the line number (or "q" to quit): ', (lineNumber) => {
+    if (lineNumber.toLowerCase() === "q") {
+      console.log("Thankyou for using the System, have a Great Day!");
+      rl.close();
+      return;
+    }
+
+    fs.readFile("poem.txt", "utf8", (err, data) => {
+      if (err) throw err;
+
+      const lines = data.split("\n");
+      const desiredLine = lines[lineNumber - 1];
+
+      if (desiredLine) {
+        console.log(`${desiredLine}`);
+      } else {
+        console.log("Line number out of range.");
+      }
+
+      promptForLineNumber();
+    });
+  });
+}
+
+promptForLineNumber();
